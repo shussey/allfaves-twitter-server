393eaf8752017e6dca0adccc7258ce5084b2dd83
Stephen Hussey <shussey@yahoo.com>
2016-11-27 10:55:37 -0800

    parameters for both services

--- routes/index.js
+++ routes/index.js
@@ -23,29 +23,11 @@ router.get('/app/tweets/:user', function(req, res, next) {
   });
 });
 
-router.get('/app/tweetsNASA', function(req, res, next) {
-  var params = {screen_name: 'NASA'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      //console.log(tweets)
-      res.send(tweets)
-    }
-  });
-});
+router.get('/app/followers/:user', function(req, res, next) {
 
-router.get('/app/tweetsLADYGAGA', function(req, res, next) {
-  var params = {screen_name: 'LADYGAGA'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      //console.log(tweets)
-      res.send(tweets)
-    }
-  });
-});
+  var user = req.params['user'];
+  var params = {screen_name: user, count: 200};
 
-
-router.get('/app/followersNASA', function(req, res, next) {
-  var params = {screen_name: 'NASA', count: 200};
   client.get('followers/list', params, function(error, followers, response){
     if(error) throw error;
     res.send(followers.users);
